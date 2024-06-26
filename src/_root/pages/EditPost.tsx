51ea606c94c3736a249e9a523348d5c6eca7803a
Palakshchat15 <palaksh1509@gmail.com>
2024-06-26 21:05:44 +0530

    first commit

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -1,28 +1,38 @@
-import PostForm from "@/components/forms/PostForm"
-import { useGetPostId } from "@/lib/react-query/queriesAndMutations";
-import { Loader } from "lucide-react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+
+import { Loader } from "@/components/shared";
+import PostForm from "@/components/forms/PostForm";
+import { useGetPostById } from "@/lib/react-query/queries";
 
 const EditPost = () => {
   const { id } = useParams();
-  const { data: post, isPending } = useGetPostId(id || '');
-  if(isPending) return <Loader />
+  const { data: post, isLoading } = useGetPostById(id);
+
+  if (isLoading)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
   return (
     <div className="flex flex-1">
       <div className="common-container">
-        <div className="max-w-5xl flex-start gap-3 justify-start w-full">
-          <img 
-          src="/assets/icons/add-post.png"
-          width={36}
-          height={36}
-          alt="add"
+        <div className="flex-start gap-3 justify-start w-full max-w-5xl">
+          <img
+            src="/assets/icons/edit.png"
+            width={36}
+            height={36}
+            alt="edit"
+            className="invert-white"
           />
           <h2 className="h3-bold md:h2-bold text-left w-full">Edit Post</h2>
         </div>
-        <PostForm action="Update" post={post} />
+
+        {isLoading ? <Loader /> : <PostForm action="Update" post={post} />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditPost
+export default EditPost;
--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,42 +1,49 @@
-import { Link, useNavigate } from 'react-router-dom'
-import { Button } from '../ui/button'
-import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
-import { useEffect } from 'react'
-import { useUserContext } from '@/context/AuthContext'
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { Button } from "../ui/button";
+import { useUserContext } from "@/context/AuthContext";
+import { useSignOutAccount } from "@/lib/react-query/queries";
 
 const Topbar = () => {
-  const { mutate: signOut, isSuccess } = useSignOutAccount();
   const navigate = useNavigate();
-  const {user} = useUserContext();
+  const { user } = useUserContext();
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+
   useEffect(() => {
-    if(isSuccess) navigate(0);
-  }, [isSuccess])
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
+
   return (
     <section className="topbar">
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
           <img
-          src="/assets/images/logo.png"
-          alt="logo"
-          width={130}
-          height={325}
+            src="/assets/images/logo.png"
+            alt="logo"
+            width={130}
+            height={325}
           />
         </Link>
+
         <div className="flex gap-4">
-          <Button variant="ghost" className="shad-button_ghost" onClick={() => signOut()}>
-            <img src="/assets/icons/logout.png" alt="logout"/>
+          <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={() => signOut()}>
+            <img src="/assets/icons/logout.png" alt="logout" />
           </Button>
           <Link to={`/profile/${user.id}`} className="flex-center gap-3">
-          <img 
-          src={user.imageUrl || '/assets/icons/profile-placeholder.png'}
-          alt="profile"
-          className='h-8 w-8 rounded-full'
-          />
+            <img
+              src={user.imageUrl || "/assets/icons/profile-placeholder.png"}
+              alt="profile"
+              className="h-8 w-8 rounded-full"
+            />
           </Link>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
